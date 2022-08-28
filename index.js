bd28adf7c654ba4145582292a1e5a2229403bcdd
otavio-n <nishimuraotavio@gmail.com>
2022-08-28 16:25:03 -0300

    Add function to send contacts to Hubspot

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-'use strict'
+"use strict";
 
+require("dotenv").config();
 const { google } = require("googleapis");
+const hubspot = require("@hubspot/api-client");
 
 async function execute() {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-      });
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
 
   const client = await auth.getClient();
 
   const googleSheets = google.sheets({ version: "v4", auth: client });
 
-  const spreadsheetId = "1f8p4SR3Qb_Pp4Yj5hlJQ5Udtwy1_jwK3gg5GR5oc8-c";
-
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
+  const spreadsheetId = process.env.GOOGLE_SHEET_ID;
 
   const getRows = await googleSheets.spreadsheets.values.get({
     auth,
@@ -25,8 +22,35 @@ async function execute() {
     range: "Página1",
   });
 
-  console.log(getRows.data)
-  
+  console.log(getRows.data);
+}
+
+async function sendContactToHubspot() {
+  const hubspotClient = new hubspot.Client({
+    accessToken: process.env.HUBSPOT_ACCESS_TOKEN,
+  });
+
+  const properties = {
+    company: "Biglytics",
+    email: "bcooper@biglytics.net",
+    firstname: "Sheldon",
+    lastname: "Cooper",
+    phone: "(877) 929-0687",
+    website: "biglytics.net",
+  };
+  const SimplePublicObjectInput = { properties };
+
+  try {
+    const apiResponse = await hubspotClient.crm.contacts.basicApi.create(
+      SimplePublicObjectInput
+    );
+    console.log(JSON.stringify(apiResponse, null, 2));
+  } catch (e) {
+    e.message === "HTTP request failed"
+      ? console.error(JSON.stringify(e.response, null, 2))
+      : console.error(e);
+  }
 }
 
 execute()
+sendContactToHubspot()
