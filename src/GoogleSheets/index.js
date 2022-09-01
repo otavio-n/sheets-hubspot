794ffe3b5ff738354626b3bcd4e4ee4e11970a6d
otavio-n <nishimuraotavio@gmail.com>
2022-08-31 22:00:52 -0300

    Refactor GoogleSheets module to class version

--- src/GoogleSheets/index.js
+++ src/GoogleSheets/index.js
@@ -2,55 +2,80 @@
 
 const { google } = require("googleapis");
 
-async function execute() {
-  const rows = await getSheetRowsFromApi();
-  await verifyFirstRow(rows[0]);
-  rows.shift();
-  return rows;
+class GoogleSheets {
+  constructor(GoogleSheetsClient, IGoogleSheetsCaller) {
+    this.googleSheetsClient = new GoogleSheetsClient()
+    this.googleSheetsCaller = new IGoogleSheetsCaller()
+  }
+
+  async execute() {
+    const {auth, client} = await this.googleSheetsClient.createGoogleClient();
+    const rowsData = await this.googleSheetsCaller.getGoogleSheetsRows(auth, client)
+    await this.verifyFirstRow(rowsData[0])
+    rowsData.shift()
+    return rowsData
+  }
+
+  async verifyFirstRow(firstRowValues) {
+    const firstRow = [
+      "Nome da empresa",
+      "Nome completo",
+      "Email",
+      "Telefone",
+      "Website",
+    ];
+  
+    if (
+      firstRow.length !== firstRowValues.length ||
+      !firstRow.every((element, index) => element === firstRowValues[index])
+    ) {
+      throw new Error("The first row is incorrect");
+    }
+  }
 }
 
-async function getSheetRowsFromApi() {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-
-  const client = await auth.getClient();
-
-  const googleSheets = google.sheets({ version: "v4", auth: client });
-
-  const spreadsheetId = process.env.GOOGLE_SHEET_ID;
-
-  try {
-    const getRows = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: "Página1",
+class GoogleSheetsClient {
+  async createGoogleClient() {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
-    console.log(JSON.stringify(getRows.data, null, 2));
-    return getRows.data.values;
-  } catch (e) {
-    e.message === "HTTP request failed"
-      ? console.error(JSON.stringify(e.response, null, 2))
-      : console.error(e);
+
+    const client = await auth.getClient();
+    return {auth, client};
   }
 }
 
-async function verifyFirstRow(firstRowValues) {
-  const firstRow = [
-    "Nome da empresa",
-    "Nome completo",
-    "Email",
-    "Telefone",
-    "Website",
-  ];
+class IGoogleSheetsCaller {
+  async getGoogleSheetsRows() {
+    throw new Error('Implement method')
+  }
+}
+class GoogleSheetsCaller extends IGoogleSheetsCaller {
+  constructor() {
+    super()
+    this.sheetId = process.env.GOOGLE_SHEET_ID;
+  }
 
-  if (
-    firstRow.length !== firstRowValues.length ||
-    !firstRow.every((element, index) => element === firstRowValues[index])
-  ) {
-    throw new Error("The first row is incorrect");
+  async getGoogleSheetsRows(auth, client) {
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const spreadsheetId = this.sheetId;
+    
+    try {
+      const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "Página1",
+      });
+      console.log(JSON.stringify(getRows.data, null, 2));
+      return getRows.data.values;
+    } catch (e) {
+      e.message === "HTTP request failed"
+        ? console.error(JSON.stringify(e.response, null, 2))
+        : console.error(e);
+    }
   }
 }
 
-module.exports = execute;
+module.exports = {GoogleSheets, GoogleSheetsClient, GoogleSheetsCaller};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 "use strict";
 
 require("dotenv").config();
-const GoogleSheets = require("./GoogleSheets");
+const {
+  GoogleSheets,
+  GoogleSheetsCaller,
+  GoogleSheetsClient,
+} = require("./GoogleSheets");
+
 const Hubspot = require("./Hubspot");
 const Parse = require("./Use-cases/integrate-contact-use-case");
 
 async function execute() {
-  const rows = await GoogleSheets();
-  const preparedContacts = await Parse(rows);
-  await Hubspot(preparedContacts);
+  const googleSheets = new GoogleSheets(GoogleSheetsClient, GoogleSheetsCaller);
+  const rowsData = await googleSheets.execute();
+  console.log(rowsData)
+  
+  // const rows = await GoogleSheets();
+  // const preparedContacts = await Parse(rows);
+  // await Hubspot(preparedContacts);
 }
 
 execute();
